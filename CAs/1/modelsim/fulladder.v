d3287471c93e7b72c98cf281a8ff22f2cf7ca604
Shahriar-0 <attarshahriar@gmail.com>
2023-03-29 00:52:48 +0430

    cleaned fa

--- CAs/1/modelsim/fulladder.v
+++ CAs/1/modelsim/fulladder.v
@@ -1,6 +1,11 @@
 `timescale 1ns/1ns
 
-module fulladder(input [N-1:0] a,b,input cin,output [N-1:0] w,output cout);
+module fulladder(a, b, cin, w, cout);
     parameter N = 4;
-    assign {cout,w} = a + b + cin;
+    input [N - 1:0] a;
+    input [N - 1:0] b;
+    input cin;
+    output [N - 1:0] w;
+    output cout;
+    assign {cout, w} = a + b + cin;
 endmodule
