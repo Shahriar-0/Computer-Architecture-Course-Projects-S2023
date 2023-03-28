c367795063e3d9bd55ab396754b65b2a0413515e
Shahriar-0 <attarshahriar@gmail.com>
2023-03-28 23:57:42 +0430

    fixed mux

--- CAs/1/modelsim/mux2in.v
+++ CAs/1/modelsim/mux2in.v
@@ -1,6 +1,11 @@
 `timescale 1ns/1ns
 
-module mux2in(input [N-1:0] a,b,input slc,output [N-1:0] w);
+module mux2in(a ,b, slc, w);
     parameter N = 4;
-    assign w = a ? ~slc : b;
+    input [N - 1:0] a;
+    input [N - 1:0] b;
+    input slc;
+    output [N - 1:0] w;
+
+    assign w = ~slc ? a : b;
 endmodule
