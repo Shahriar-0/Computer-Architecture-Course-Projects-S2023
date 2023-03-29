9dd2d1f7ab232a5e98a0e7dbc25542ab363f17ba
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2023-03-30 00:20:37 +0430

    fix maze memory error

--- CAs/1/code/maze_memory.v
+++ CAs/1/code/maze_memory.v
@@ -6,15 +6,19 @@ module maze_memory(X, Y, D_in, RD, WR, D_out);
 
     input [N - 1:0] X, Y;
     input D_in, RD, WR;
-    output D_out;
+    output reg D_out;
 
     reg [0:WIDTH - 1] maze [0:HEIGHT - 1];
 
     initial begin
         $readmemh(FILENAME, maze);
     end
-    
+
     assign D_out = (RD) ? maze[X][Y] : D_out;
-    assign maze[X][Y] = (WR) ? D_in : maze[X][Y];
+
+    always @(WR) begin 
+        if(WR)
+            maze[X][Y] = D_in;
+    end
 
 endmodule
