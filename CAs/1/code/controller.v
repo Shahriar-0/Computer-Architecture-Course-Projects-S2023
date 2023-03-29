6334f59a4456d9cea79dbd3fe49b575eb06d36af
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2023-03-29 23:41:54 +0430

    fix error controllers

--- CAs/1/code/controller.v
+++ CAs/1/code/controller.v
@@ -21,8 +21,8 @@
 
 module controller(CLK, RST, start, Run, Co, found, stack_empty, Move,
 				  complete_read, D_out, init_x, init_y, init_count,
-				  en_count, ldc, ldx, ldy, WR, RD, D_in, stack_pop,
-				  stack_push, list_push, en_read, init_list, Done, Fail);
+				  en_count, ldc, ldx, ldy, WR, RD, D_in,init_stack, stack_pop,
+				  stack_push, r_update, list_push, en_read, init_list, Done, Fail);
 
 	input CLK, RST, start, Run, Co, found,
 		  stack_empty, complete_read, D_out;
@@ -30,34 +30,35 @@ module controller(CLK, RST, start, Run, Co, found, stack_empty, Move,
 	output init_x, init_y, init_count, en_count,
 		   ldc, ldx, ldy, WR, RD, D_in, Done, 
 		   list_push, en_read, init_list, Fail,
-		   stack_push, stack_pop, Move;
+		   init_stack,stack_push, stack_pop,
+		   r_update, Move;
 		   
 
 	wire [4:0] pstate = `idle;
 	wire [4:0] nstate;
 
 
-	always @(Run or start or Co or pstate or D_out or complete_read or empty_stack or found) begin
+	always @(Run or start or Co or pstate or D_out or complete_read or stack_empty or found) begin
 		case (pstate)
-			`idle:                ns <= ~start? `idle : `init;                        
-			`init:                ns <= ~start? `init_search : `init;                        
-			`init_search:         ns <= `add_to_stack;                        
-			`add_to_stack:        ns <= `make_wall;                       
-			`make_wall:           ns <= `update_xy;                        
-			`update_xy:           ns <= `check_goal;                        
-			`check_goal:          ns <= found ? `stack_read : `check_wall;                       
-			`check_wall:          ns <= D_out ? `check_empty_stack : `init_search;                        
-			`check_empty_stack:   ns <= empty_stack ? `fail : `pop_stack;                        
-			`pop_stack:           ns <= `reload_counter;                         
-			`reload_counter:      ns <= `update_reverse;                        
-			`update_reverse:      ns <= `free_loc_check_bt;                       
-			`free_loc_check_bt:   ns <= Co ? `fail : `pop_stack;                       
-			`change_dir:          ns <= `add_to_stack;                        
-			`fail:                ns <= `fail;                        
-			`stack_read:          ns <= `update_list;                       
-			`update_list:         ns <= ~empty_stack ? `stack_read : `done;                        
-			`done:                ns <= Run ? `show : `done;                        
-			`show:                ns <= complete_read ? `done : `show;            
+			`idle:                nstate <= ~start? `idle : `init;                        
+			`init:                nstate <= ~start? `init_search : `init;                        
+			`init_search:         nstate <= `add_to_stack;                        
+			`add_to_stack:        nstate <= `make_wall;                       
+			`make_wall:           nstate <= `update_xy;                        
+			`update_xy:           nstate <= `check_goal;                        
+			`check_goal:          nstate <= found ? `stack_read : `check_wall;                       
+			`check_wall:          nstate <= D_out ? `check_empty_stack : `init_search;                        
+			`check_empty_stack:   nstate <= stack_empty ? `fail : `pop_stack;                        
+			`pop_stack:           nstate <= `reload_counter;                         
+			`reload_counter:      nstate <= `update_reverse;                        
+			`update_reverse:      nstate <= `free_loc_check_bt;                       
+			`free_loc_check_bt:   nstate <= Co ? `fail : `pop_stack;                       
+			`change_dir:          nstate <= `add_to_stack;                        
+			`fail:                nstate <= `fail;                        
+			`stack_read:          nstate <= `update_list;                       
+			`update_list:         nstate <= ~stack_empty ? `stack_read : `done;                        
+			`done:                nstate <= Run ? `show : `done;                        
+			`show:                nstate <= complete_read ? `done : `show;            
 		endcase
 	end
 
@@ -69,19 +70,19 @@ module controller(CLK, RST, start, Run, Co, found, stack_empty, Move,
 			`idle: ;             
 			`init: {init_x, init_count, init_y, init_list, init_stack} = 5'b1;             
 			`init_search: init_count = 1'b1;      
-			`add_to_stack: stack_dir_push = 1'b1;      
+			`add_to_stack: stack_push = 1'b1;      
 			`make_wall: {WR, D_in} = 2'b1;        
 			`update_xy: ;       
 			`check_goal: ;       
 			`check_wall: RD = 1'b1;       
 			`check_empty_stack: ;
-			`pop_stack: stack_dir_pop = 1'b1;        
-			`reload_counter: ld_count = 1'b1;   
-			`update_reverse: {ld_x, ld_y, r_update} = 3'b1;  
+			`pop_stack: stack_pop = 1'b1;        
+			`reload_counter: ldc = 1'b1;   
+			`update_reverse: {ldx, ldy, r_update} = 3'b1;  
 			`free_loc_check_bt: WR = 1'b1;
 			`change_dir: en_count = 1'b1;       
 			`fail: Fail = 1'b1;             
-			`stack_read: stack_dir_pop = 1'b1;      
+			`stack_read: stack_pop = 1'b1;      
 			`update_list: list_push = 1'b1;      
 			`done: {Done, en_read} = 2'b1;            
 			`show: en_read = 1'b1;            
@@ -90,7 +91,7 @@ module controller(CLK, RST, start, Run, Co, found, stack_empty, Move,
 
 	always @(posedge CLK or posedge RST) begin
 		if (RST)
-			pstate <= idle;
+			pstate <= `idle;
 		else 
 			pstate <= nstate;
 	end
