4d7b03cecc57f300ae8c9cd57cbb1431271eaf4c
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2023-03-31 17:15:04 +0430

    change fulladder to inc_dec

--- CAs/1/code/fulladder.v
+++
@@ -1,9 +0,0 @@
-module fulladder(a, b, cin, w, cout);
-    parameter N = 4;
-    input [N - 1:0] a, b;
-    input cin;
-    
-    output [N - 1:0] w;
-    output cout;
-    assign {cout, w} = a + b + cin;
-endmodule
