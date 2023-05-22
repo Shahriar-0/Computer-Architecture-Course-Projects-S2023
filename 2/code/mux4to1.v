d50564ee454ba0a5ee2c306a0c32c674cb6cf471
Shahriar <77535132+Shahriar-0@users.noreply.github.com>
2023-05-22 18:45:52 +0330

    fixed assign problem in mux

--- 2/code/mux4to1.v
+++ 2/code/mux4to1.v
@@ -2,13 +2,13 @@ module mux2to1(slc, a, b, c, d, w);
     parameter N = 32;
 
     input [1:0] slc;
-    input [N-1:0] a, b , c, d;
+    input [N-1:0] a, b, c, d;
     
     output [N-1:0] w;
 
-    w = (slc == 2'b00) ? a :
-        (slc == 2'b01) ? b :
-        (slc == 2'b10) ? c :
-        (slc == 2'b11) ? d : 3'bz;
+    assign w = (slc == 2'b00) ? a :
+               (slc == 2'b01) ? b :
+               (slc == 2'b10) ? c :
+               (slc == 2'b11) ? d : 1'bz;
 
 endmodule
