b2a0978b8305e2d12feef76064b14fc857ed6851
Shahriar <77535132+Shahriar-0@users.noreply.github.com>
2023-05-22 18:39:50 +0330

    cleaned mux4to1

--- 2/code/mux4to1.v
+++ 2/code/mux4to1.v
@@ -1,14 +1,14 @@
-module mux2to1(slc , a, b , c, d, w);
+module mux2to1(slc, a, b, c, d, w);
+    parameter N = 32;
+
     input [1:0] slc;
     input [N-1:0] a, b , c, d;
     
     output [N-1:0] w;
 
-    parameter N = 32;
+    w = (slc == 2'b00) ? a :
+        (slc == 2'b01) ? b :
+        (slc == 2'b10) ? c :
+        (slc == 2'b11) ? d : 3'bz;
 
-    w = (slc == 2'b00 ) ?  a :
-        (slc == 2'b01 ) ?  b :
-        (slc == 2'b10 ) ? c :
-        (slc == 2'b11 ) ? d :
-        3'bz;
 endmodule
