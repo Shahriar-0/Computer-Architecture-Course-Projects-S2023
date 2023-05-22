e40fae60ffd3a51ba310b824e8cbf94f03b9db61
Shahriar <77535132+Shahriar-0@users.noreply.github.com>
2023-05-22 18:37:05 +0330

    fixed alu

--- 2/code/ALU.v
+++ 2/code/ALU.v
@@ -1,14 +1,18 @@
-module ALU(ALUcntrl, A, B, zero,  W);
+module ALU(ALUcntrl, A, B, zero, W);
+    parameter N = 32;
+
     input [2:0] ALUcntrl;
     input [N-1:0] A;
-    input [N-1:0] B ,output zero;
+    input [N-1:0] B;
+    
+    output zero;
     output [N-1:0] W;
-    parameter N = 32;
-    w = ( ALUcntrl == 3'b000 ) ?  A + B :
-        ( ALUcntrl == 3'b001 ) ?  A -B :
-        ( ALU cntrl == 3'b010 ) ?  A & B :
-        ( ALU cntrl == 3'b011 ) ? A | B :
-        ( ALU cntrl == 3'b101 ) ? :
-        3'bz;
+    w = (ALUcntrl  == 3'b000) ?  A + B :
+        (ALUcntrl  == 3'b001) ?  A - B :
+        (ALU cntrl == 3'b010) ?  A & B :
+        (ALU cntrl == 3'b011) ?  A | B :
+        (ALU cntrl == 3'b101) ? : 3'bz;
+    
     zero = ( ~|w );
+    
 endmodule
