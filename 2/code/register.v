3c32e68d46421debc7bae48c7fe322563c623079
Shahriar <77535132+Shahriar-0@users.noreply.github.com>
2023-05-22 18:41:14 +0330

    fixed register file

--- 2/code/register.v
+++ 2/code/register.v
@@ -1,12 +1,12 @@
 module register(clk, a, w);
+    parameter N = 32;
+
     input clk;
     input [N-1:0] a;
 
     output reg [N-1:] w;
-    
-    parameter N = 32;
 
-    always @(posedge clk) begin
+    always @(posedge clk) 
         w <= a;
-    end
+    
 endmodule
--- 2/code/registerfile.v
+++ 2/code/registerfile.v
@@ -1,23 +1,24 @@
 module registerFile(clk, regWrite, A1, A2, A3, WD, RD1, RD2);
+    parameter RN = 5;
+    parameter N = 32;
+
     input clk, regWrite;
     input [RN-1:0] A1, A2, A3;
     input [N-1:0] WD;
     
     output [N-1:0] RD1, RD2;
 
-    parameter RN = 5;
-    parameter N = 32;
-
     reg [N-1:0] registers [0:N-1];
 
-    initial begin
+    initial 
         registers[0] = 32'd0;
-    end
+    
     assign ReadData1 = (ReadRegister1 == 5'd0) ? 32'd0 : registers[ReadRegister1];
     assign ReadData2 = (ReadRegister2 == 5'd0) ? 32'd0 : registers[ReadRegister2];
 
-    always @(posedge clk)begin
+    always @(posedge clk) begin
         if(regWrite && A3 != 5'b0)
             registers[A3] <= WD;
-        end
+    end
+
 endmodule
