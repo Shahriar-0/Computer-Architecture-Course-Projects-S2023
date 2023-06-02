c49af6ad1c34f86899ae9a55c45ae85e85319fa2
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2023-06-02 16:17:11 +0330

    add rst signal

--- 3/code/register.v
+++ 3/code/register.v
@@ -1,12 +1,14 @@
-module Register(in, en, clk, out);
+module Register(in, en, rst, clk, out);
     parameter N = 32;
 
     input [N-1:0] in;
-    input en, clk;
+    input en, clk, rst;
 
     output reg [N-1:0] out;
 
-    always @(posedge clk) begin
+    always @(posedge clk, posedge rst) begin
+        if(rst)
+            out <= {N{1'b0}};
         else if (en)
             out <= in;
     end
