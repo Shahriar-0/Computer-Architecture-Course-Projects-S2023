a3ee7b5ac6c76166362ce1255e33358cc97694bf
Shahriar-0 <attarshahriar@gmail.com>
2023-06-02 12:02:58 +0330

    gets instead of equal

--- 2/code/mux4to1.v
+++ 2/code/mux4to1.v
@@ -8,11 +8,11 @@ module Mux4to1(slc, a, b, c, d, w);
 
     always @(a or b or c or d or slc) begin
         case (slc)
-            2'b00  : w = a;
-            2'b01  : w = b;
-            2'b10  : w = c;
-            2'b11  : w = d;
-            default: w = {N{1'bz}};
+            2'b00  : w <= a;
+            2'b01  : w <= b;
+            2'b10  : w <= c;
+            2'b11  : w <= d;
+            default: w <= {N{1'bz}};
         endcase
     end
 
