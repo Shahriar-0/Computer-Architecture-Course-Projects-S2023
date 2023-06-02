5754dd0f4c17d5e7dfc85dc36bd964e5dcc66ed6
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2023-06-02 14:45:43 +0330

    check immExtension

--- 3/code/ImmediateExtend.v
+++ 3/code/ImmediateExtend.v
@@ -1,18 +1,24 @@
 `define I_T 3'b000
 `define S_T 3'b001
-`define B_T 3'b010
-`define J_T 3'b011
+`define J_T 3'b010
+`define B_T 3'b011
 `define U_T 3'b100
 
 module ImmExtension(immSrc, data, w);
 
     input [2:0] immSrc;
     input [24:0] data;
-    assign w = (immSrc == `I_T) ? {20{1'b{data[31]}},data[31:20]}:
-                (immSrc == `S_T) ? {20{1'b{data[31]}},data[31:25], data[11:7]}:
-                (immSrc == `B_T) ? {20{1'b{data[31]}},data[31], data[11], data[30:25], data[10:7], 1'b0};
-                (immSrc == `U_T) ? {data[31:12], 12{1'b0}}:
-                (immSrc == `J_T) ? {12{1'b{data[31]}}, data[31], data[19:12], data[20], data[30:21], 1'b0}:
-                32'b0;
+
+    always @(immSrc) begin
+        case(immSrc) begin
+            `I_T: w <= {20{data[24]}, data[24:13]};
+            `S_T: w <= {20{data[24]}, data[24:18], data[4:0]};
+            `J_T: w <= {12{data[24]}, data[12:5], data[13], data[23:14], 1'b0};
+            `B_T: w <= {20{data[24]}, data[7], data[23:18], data[4:1], 1'b0};
+            `U_T: w <= {data[24:5], 12{1'b0}};
+            default: w <= 32'b0;
+        end
+        endcase
+    end
 
 endmodule
