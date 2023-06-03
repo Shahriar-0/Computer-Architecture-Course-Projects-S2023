00a8c91447cdbddc42260ce94319b2f55d6d91c9
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2023-06-04 00:22:48 +0330

    change controllers based on riscV opcode

--- 2 - RISC-V Single-Cycle/code/MainController.v
+++ 2 - RISC-V Single-Cycle/code/MainController.v
@@ -1,45 +1,56 @@
-`define R_T 7'd0
-`define I_T 7'd1
-`define S_T 7'd2
-`define B_T 7'd3
-`define U_T 7'd4
-`define J_T 7'd5
+`define R_T     7'b0110011
+`define I_T     7'b0010011
+`define S_T     7'b0100011
+`define B_T     7'b1100011
+`define U_T     7'b0110111
+`define J_T     7'b1101111
+`define LW_T    7'b0000011
+`define JALR_T  7'b1100111
 
-`define LW 3'b110
-`define JALR 3'b111
-
-module MainController(op, func3, func7, zero,
-                      resultSrc, memWrite, ALUOp,
-                      ALUSrc, immSrc, regWrite, 
+module MainController(op, zero, resultSrc, memWrite,
+                      ALUOp, ALUSrc, immSrc, regWrite, 
                       jal, jalr, neg, branch);
 
     input [6:0] op;
-    input [2:0] func3;
-    input [6:0] func7;
     input zero, neg;
 
     output reg [1:0] resultSrc, ALUOp;
     output reg [2:0] immSrc;
     output reg regWrite, memWrite, ALUSrc, jal, jalr, branch;
     
-    always @(op, func3) begin 
+    always @(op) begin 
         {memWrite, regWrite, ALUSrc, jal, 
             jalr, branch, immSrc, resultSrc, ALUOp} <= 13'b0;
         case(op)
-
             `R_T:begin
                 ALUOp     <= 2'b10;
                 regWrite  <= 1'b1;
             end
 
             `I_T:begin
-                ALUOp     <= 2'b10;
+                ALUOp     <= 2'b11;
                 regWrite  <= 1'b1;
                 immSrc    <= 3'b000;
                 ALUSrc    <= 1'b1;
-                jalr      <= (func3 == `JALR) ? 1'b1  : 1'b0;
-                resultSrc <= (func3 == `JALR) ? 2'b10 :
-                             (func3 == `LW)   ? 2'b01 : 2'b00;
+                jalr      <= 1'b0;
+                resultSrc <= 2'b00;
+            end
+            `LW_T:begin
+                ALUOp     <= 2'b00;
+                regWrite  <= 1'b1;
+                immSrc    <= 3'b000;
+                ALUSrc    <= 1'b1;
+                jalr      <= 1'b0;
+                resultSrc <= 2'b01;
+            end
+
+            `JALR_T:begin
+                ALUOp     <= 2'b00;
+                regWrite  <= 1'b1;
+                immSrc    <= 3'b000;
+                ALUSrc    <= 1'b1;
+                jalr      <= 1'b1;
+                resultSrc <= 2'b10;
             end
 
             `S_T:begin
@@ -67,8 +78,6 @@ module MainController(op, func3, func7, zero,
                 immSrc    <= 3'b100;
                 regWrite  <= 1'b1;
             end
-
-
         endcase
     end
 endmodule
