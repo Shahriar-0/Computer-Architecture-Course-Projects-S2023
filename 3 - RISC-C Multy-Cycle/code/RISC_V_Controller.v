0efd682c7561427d157a8f63ef4bf505321961e1
Shahriar-0 <attarshahriar@gmail.com>
2023-06-03 21:05:35 +0330

    fixed datapath

--- 3 - RISC-C Multy-Cycle/code/RISC_V_Controller.v
+++ 3 - RISC-C Multy-Cycle/code/RISC_V_Controller.v
@@ -2,35 +2,38 @@ module CPU_Controller(clk, rst, op, func3, func7, zero, neg,
                     PCWrite, AdrSrc, MemWrite,
                     IRWrite, resultSrc, ALUControl,
                     ALUSrcA, ALUSrcB, immSrc, RegWrite);
-        input [6:0] op;
-        input [6:0] func7;
-        input [2:0] func3;
-        input clk, rst, zero , neg;
-        
-        output PCWrite, AdrSrc, MemWrite, IRWrite, RegWrite;
-        output [1:0] resultSrc, ALUSrcA, ALUSrcB;
-        output [2:0] ALUControl, immSrc;
-        wire PCUpdate, branch, branchRes;
-        wire[1:0] ALUOp;
+    input [6:0] op;
+    input [6:0] func7;
+    input [2:0] func3;
+    input clk, rst, zero , neg;
+    
+    output PCWrite, AdrSrc, MemWrite, IRWrite, RegWrite;
+    output [1:0] resultSrc, ALUSrcA, ALUSrcB;
+    output [2:0] ALUControl, immSrc;
+    
+    wire PCUpdate, branch, branchRes;
+    wire[1:0] ALUOp;
 
-        MainController MainDecoder(
-                .clk(clk), .rst(rst), .op(op), .func3(func3), .func7(func7),
-                .zero(zero), .neg(neg), .PCUpdate(PCUpdate), .AdrSrc(AdrSrc),
-                .MemWrite(MemWrite), .branch(branch), .IRWrite(IRWrite),
-                .resultSrc(resultSrc), .ALUOp(ALUOp), .ALUSrcA(ALUSrcA),
-                .ALUSrcB(ALUSrcB), .immSrc(immSrc), .RegWrite(RegWrite)
-        );
-        
-        ALU_Controller ALUDecoder(
-                .func3(func3), .ALUOp(ALUOp), .ALUControl(ALUControl)
-        );
-
-        
-        BranchController BranchDecoder(
-                .func3(func3), .branch(branch), .neg(neg), .zero(zero), .w(branchRes)
-        ); 
-                                             
-        assign PCWrite = (PCUpdate | branchRes);
-
+    MainController MainDecoder(
+        .clk(clk), .rst(rst), .op(op),
+        .zero(zero), .neg(neg), .func3(func3),
+        .PCUpdate(PCUpdate), .AdrSrc(AdrSrc),
+        .MemWrite(MemWrite), .branch(branch),
+        .resultSrc(resultSrc), .ALUSrcA(ALUSrcA),
+        .ALUSrcB(ALUSrcB), .immSrc(immSrc), 
+        .RegWrite(RegWrite), .IRWrite(IRWrite), 
+        .ALUOp(ALUOp), .func7(func7)
+    );
+    
+    ALU_Controller ALUDecoder(
+        .func3(func3), .ALUOp(ALUOp), .ALUControl(ALUControl)
+    );    
+    
+    BranchController BranchDecoder(
+        .func3(func3), .branch(branch), 
+        .neg(neg), .zero(zero), .w(branchRes)
+    ); 
+                                         
+    assign PCWrite = (PCUpdate | branchRes);    
 
 endmodule
--- 3 - RISC-C Multy-Cycle/code/RISC_V_Datapath.v
+++ 3 - RISC-C Multy-Cycle/code/RISC_V_Datapath.v
@@ -6,34 +6,50 @@ module CPU_Datapath(clk, rst, PCWrite, AdrSrc, MemWrite,
     input clk, rst, PCWrite, AdrSrc, MemWrite, IRWrite, RegWrite;
     input [1:0] resultSrc, ALUSrcA, ALUSrcB;
     input [2:0] ALUControl, immSrc;
+    // op, func3, func7, zero, neg
+    // those are not defined
 
     wire [31:0] PC, Adr, ReadData, OldPC;
     wire [31:0] ImmExt, Instr, Data, ALUResult;
     wire [31:0] RD1, RD2, A, B, SrcA, SrcB;
     wire [31:0] ALUResult, ALUOut,Result;
 
-    Register PCR(.in(Result), .en(PCWrite), .rst(rst), .clk(clk), .out(PC));
-    Register OldPCR(.in(PC), .en(IRWrite), .rst(1'b0), .clk(clk), .out(OldPC));
-    Register IR(.in(ReadData), .en(IRWrite), .rst(1'b0), .clk(clk), .out(Instr));
-    Register MDR(.in(ReadData), .en(1'b1), .rst(1'b0), .clk(clk), .out(Data));
-    Register AR(.in(RD1), .en(1'b1), .rst(1'b0), .clk(clk), .out(A));
-    Register BR(.in(RD2), .en(1'b1), .rst(1'b0), .clk(clk), .out(B));
-    Register ALUR(.in(ALUResult), .en(1'b1), .rst(1'b0), .clk(clk), .out(ALUOut));
+    Register PCR   (.in(Result),    .en(PCWrite), .rst(rst),  .clk(clk), .out(PC));
+    Register OldPCR(.in(PC),        .en(IRWrite), .rst(1'b0), .clk(clk), .out(OldPC));
+    Register IR    (.in(ReadData),  .en(IRWrite), .rst(1'b0), .clk(clk), .out(Instr));
+    Register MDR   (.in(ReadData),  .en(1'b1),    .rst(1'b0), .clk(clk), .out(Data));
+    Register AR    (.in(RD1),       .en(1'b1),    .rst(1'b0), .clk(clk), .out(A));
+    Register BR    (.in(RD2),       .en(1'b1),    .rst(1'b0), .clk(clk), .out(B));
+    Register ALUR  (.in(ALUResult), .en(1'b1),    .rst(1'b0), .clk(clk), .out(ALUOut));
 
-    mux2to1 AdrMux(.slc(AdrSrc), .a(PC), .b(Result), w(Adr));
-    mux4to1 AMux(.slc(ALUSrcA), .a(PC), .b(OldPC), .c(A), .d(32'd0), .w(SrcA));
-    mux4to1 BMux(.slc(ALUSrcB), .a(B), .b(ImmExt), .c(32'd4), .d(32'd0), .w(SrcB));
-    mux4to1 BMux(.slc(resultSrc), .a(ALUOut), .b(Data), .c(ALUResult), .d(ImmExt), .w(Result));
+    mux2to1 AdrMux(.slc(AdrSrc),    .a(PC),     .b(Result), .w(Adr));
 
-    ImmExtension Extend(.immSrc(immSrc), .data(PC[31:7]), .w(ImmExt));
+    mux4to1 AMux  (.slc(ALUSrcA),   .a(PC),     .b(OldPC),  .c(A),         .d(32'd0),  .w(SrcA));
+    mux4to1 BMux  (.slc(ALUSrcB),   .a(B),      .b(ImmExt), .c(32'd4),     .d(32'd0),  .w(SrcB));
+    mux4to1 BMux  (.slc(resultSrc), .a(ALUOut), .b(Data),   .c(ALUResult), .d(ImmExt), .w(Result));
 
-    ALU Alu(.opc(ALUControl), .a(SrcA), .b(SrcB), .zero(zero), .neg(neg), .w(ALUResult));
+    ImmExtension Extend(
+        .immSrc(immSrc), .data(PC[31:7]), .w(ImmExt)
+    );
 
-    InstrDataMemory DM(.memAdr(Adr), .writeData(B), .clk(clk), .memWrite(MemWrite), .readData(ReadData));
+    ALU Alu(
+        .opc(ALUControl), .a(SrcA), .b(SrcB), 
+        .zero(zero), .neg(neg), .w(ALUResult)
+    );
 
-    RegisterFile RF(.clk(clk), .regWrite(RegWrite),
-                    .readRegister1(Instr[19:15]), .readRegister2(Instr[[24:20]]),
-                    .writeRegister(Instr[11:7]), .writeData(Result),
-                    .readData1(RD1), .readData2(RD2));
+    InstrDataMemory DM(
+        .memAdr(Adr), .writeData(B), .clk(clk), 
+        .memWrite(MemWrite), .readData(ReadData)
+    );
+
+    RegisterFile RF(
+        .clk(clk), 
+        .regWrite(RegWrite),
+        .writeData(Result), 
+        .readData1(RD1), .readData2(RD2),
+        .readRegister1(Instr[19:15]), 
+        .readRegister2(Instr[[24:20]]),
+        .writeRegister(Instr[11:7]), 
+    );
 
 endmodule
