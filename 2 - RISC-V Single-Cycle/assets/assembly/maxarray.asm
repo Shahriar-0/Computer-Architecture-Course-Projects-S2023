b1be87e8cc368663dde0a434a298fedcfc93f0ec
Shahriar-0 <attarshahriar.com>
2023-06-04 00:45:33 +0330

    added global

--- 2 - RISC-V Single-Cycle/assets/assembly/maxarray.asm
+++ 2 - RISC-V Single-Cycle/assets/assembly/maxarray.asm
@@ -1,22 +1,26 @@
-jal x0, FindMax
+.global _boot
 
-FindMax:
-    lw   x8, 1000(x0)           # maxElement = mem[1000]
-    add  x9, x0, x0             # i
+.text
+_boot:
+    jal x0, FindMax
 
-    Loop:
-        addi x9, x9, 4          # i += 4
-        slti x6, x9, 40         # check if 10 elements are traversed (40 = 4 * 10)
-        beq  x6, x0, EndLoop    # if 10 elements are traversed, jump to EndLoop
-        lw   x18, 1000(x9)      # element = mem[i]
-        slt  x6, x8, x18        # check if element is greater than maxElement
-        beq  x6, x0, Loop       # if element is not greater than maxElement, jump to Loop
-        add  x8, x18, x0        # maxElement = element
-        jal  x0, Loop           # jump to Loop
+    FindMax:
+        lw   x8, 1000(x0)           # maxElement = mem[1000]
+        add  x9, x0, x0             # i
 
-    EndLoop:
-        sw x8, 2000(x0)         # mem[2000] = maxElement
-        jal x0, End             # return
+        Loop:
+            addi x9, x9, 4          # i += 4
+            slti x6, x9, 40         # check if 10 elements are traversed (40 = 4 * 10)
+            beq  x6, x0, EndLoop    # if 10 elements are traversed, jump to EndLoop
+            lw   x18, 1000(x9)      # element = mem[i]
+            slt  x6, x8, x18        # check if element is greater than maxElement
+            beq  x6, x0, Loop       # if element is not greater than maxElement, jump to Loop
+            add  x8, x18, x0        # maxElement = element
+            jal  x0, Loop           # jump to Loop
 
-End:
-    jal x1, FindMax                 # call FindMax
+        EndLoop:
+            sw x8, 2000(x0)         # mem[2000] = maxElement
+            jal x0, End             # return
+
+    End:
+        jal x1, FindMax                 # call FindMax
