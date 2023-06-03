b947587b0f6c1e950b0bc9e5847359e40897cbf9
Shahriar-0 <attarshahriar@gmail.com>
2023-06-03 21:09:05 +0330

    fixed cpu

--- 3 - RISC-C Multy-Cycle/code/RISC_V.v
+++ 3 - RISC-C Multy-Cycle/code/RISC_V.v
@@ -2,22 +2,34 @@ module RISC_V(clk, rst);
     input clk, rst;
     
     wire [2:0] func3, ALUControl, immSrc;
-    wire zero, neg, PCSrc, memWrite, regWrite, ALUSrc , PCWrite, AdrSrc IRWrite;
+
+    wire zero, neg, PCSrc, memWrite, 
+         regWrite, ALUSrc, PCWrite, AdrSrc IRWrite;
+
     wire [1:0] resultSrc, ALUSrcA, ALUSrcB;
+    
     wire [6:0] op, func7; 
 
     CPU_Controller CPU(
-        .clk(clk), .rst(rst), .op(op), .func3(func3),
-        .func7(func7), .zero(zero), .neg(neg), .PCWrite(PCWrite),
-        .AdrSrc(AdrSrc), .MemWrite(MemWrite), .IRWrite(IRWrite),
-        .resultSrc(resultSrc), .ALUControl(ALUControl), .ALUSrcA(ALUSrcA),
-        .ALUSrcB(ALUSrcB), .immSrc(immSrc), .RegWrite(RegWrite)
+        .clk(clk), .rst(rst), .op(op), 
+        .func3(func3), .immSrc(immSrc), 
+        .func7(func7), .zero(zero), 
+        .neg(neg), .PCWrite(PCWrite),
+        .AdrSrc(AdrSrc), .MemWrite(MemWrite), 
+        .IRWrite(IRWrite), .resultSrc(resultSrc), 
+        .ALUControl(ALUControl), .ALUSrcA(ALUSrcA),
+        .ALUSrcB(ALUSrcB), .RegWrite(RegWrite)
     );
+
     CPU_Datapath DP(
-        .clk(clk), .rst(rst), .PCWrite(PCWrite), .AdrSrc(AdrSrc),
-        .MemWrite(MemWrite), .IRWrite(IRWrite), .resultSrc(resultSrc),
-        .ALUControl(ALUControl), .ALUSrcA(ALUSrcA), .ALUSrcB(ALUSrcB),
-        .immSrc(immSrc), .RegWrite(RegWrite), .op(op), .func3(func3),
-        .func7(func7), .zero(zero), .neg(neg)
-        );
+        .clk(clk), .rst(rst), .neg(neg),
+        .PCWrite(PCWrite), .AdrSrc(AdrSrc),
+        .MemWrite(MemWrite), .IRWrite(IRWrite), 
+        .resultSrc(resultSrc), .immSrc(immSrc), 
+        .ALUControl(ALUControl), .op(op),
+        .ALUSrcA(ALUSrcA), .func3(func3),
+        .ALUSrcB(ALUSrcB), .zero(zero),
+        .RegWrite(RegWrite), .func7(func7)
+    );
+    
 endmodule
