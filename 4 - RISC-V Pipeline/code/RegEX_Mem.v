b8f1d5ee3d7ad521c30a0a50b2fb54fcef3deebc
Shahriar-0 <attarshahriar.com>
2023-06-07 12:19:01 +0330

    ex mem

--- 4 - RISC-V Pipeline/code/RegEX_Mem.v
+++ 4 - RISC-V Pipeline/code/RegEX_Mem.v
@@ -1,3 +1,27 @@
-module RegEX_MEM();
+module RegEX_MEM(clk, rst, ALUResult, memData, memDataOut,
+                 RD, RDOut, memToReg, memToRegOut);
+
+    input clk, rst;
+    input [31:0] ALUResult, memData;
+    input [4:0] RD;
+    input [2:0] memToReg;
+    
+    output reg [31:0] AlUResultOut, memDataOut;
+    output reg [4:0] RDOut;
+    output reg [2:0] memToRegOut;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            ALUResultOut <= 32'b0;
+            memDataOut <= 32'b0;
+            RDOut <= 5'b0;
+            memToRegOut <= 3'b0;
+        end else begin
+            ALUResultOut <= ALUResult;
+            memDataOut <= memData;
+            RDOut <= RD;
+            memToRegOut <= memToReg;
+        end
+    end
 
 endmodule
