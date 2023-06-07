6d98725262a7b3741f14d8cdce5f50c09651043b
Shahriar-0 <attarshahriar.com>
2023-06-07 12:37:10 +0330

    main controller

--- 4 - RISC-V Pipeline/code/ForwardingUnit.v
+++ 4 - RISC-V Pipeline/code/ForwardingUnit.v
@@ -16,21 +16,7 @@ module ForwardingUnit(EX_MEM_RegWrite, EX_MEM_rd, EX_MEM_ALUResult,
     assign ex_mem_forward_rs2 = (EX_MEM_RegWrite && (EX_MEM_rd != 0) && (EX_MEM_rd == ID_EX_rs2));
     assign mem_wb_forward_rs1 = (MEM_WB_RegWrite && (MEM_WB_rd != 0) && !MEM_WB_MemToReg && (MEM_WB_rd == ID_EX_rs1));
     assign mem_wb_forward_rs2 = (MEM_WB_RegWrite && (MEM_WB_rd != 0) && !MEM_WB_MemToReg && (MEM_WB_rd == ID_EX_rs2;    
-    
-    always @(*) begin
-        if (ex_mem_forward_rs1)
-            forwarded_rs1 = EX_MEM_ALUResult;
-        else if (mem_wb_forward_rs1)
-            forwarded_rs1 = MEM_WB_ALUResult;
-        else
-            forwarded_rs1 = ID_EX_rs1;
-
-        if (ex_mem_forward_rs2)
-            forwarded_rs2 = EX_MEM_ALUResult;
-        else if (mem_wb_forward_rs2)
-            forwarded_rs2 = MEM_WB_ALUResult;
-        else
-            forwarded_rs2 = ID_EX_rs2;
-    end
+    assign forwarded_rs1 = (ex_mem_forward_rs1 || mem_wb_forward_rs1)? MEM_WB_ALUResult : ID_EX_rs1;
+    assign forwarded_rs2 = (ex_mem_forward_rs2 || mem_wb_forward_rs2)? MEM_WB_ALUResult : ID_EX_rs2;
 
 endmodule
--- 4 - RISC-V Pipeline/code/HazardUnit.v
+++ 4 - RISC-V Pipeline/code/HazardUnit.v
@@ -10,17 +10,16 @@ module HazardUnit(ID_EX_MemRead, ID_EX_rs1, ID_EX_rs2, IF_ID_rs1,
 
     wire rs1_hazard, rs2_hazard, mem_hazard;
 
-    assign rs1_hazard = ((ID_EX_MemRead && (ID_EX_rs1 != 0) && ((ID_EX_rs1 == IF_ID_rs1) || (_EX_rs1 == IF_ID_rs2))) ||
-                         (IF_ID_MemRead && ((IF_ID_rs1 == IF_ID_rs2) && (IF_ID_rs1 != 0))));
-    assign rs2_hazard = ((ID_EX_MemRead && (ID_EX_rs2 != 0) && ((ID_EX_rs2 == IF_ID_rs1) || (ID_EX_rs2 == IF_ID_rs2))) ||
-                         (IF_ID_MemRead && ((IF_ID_rs1 == IF_ID_rs2) && (IF_ID_rs2 != 0))));
-    assign mem_hazard = (IF_ID_MemRead && ((IF_ID_rs1 == IF_ID_rs2) && (IF_ID_rs1 !=0)));
+    assign rs1_hazard = ((ID_EX_MemRead && (ID_EX_rs1 != 0) && 
+                        ((ID_EX_rs1 == IF_ID_rs1) || (_EX_rs1 == IF_ID_rs2))) ||
+                        (IF_ID_MemRead && ((IF_ID_rs1 == IF_ID_rs2) && (IF_ID_rs1 != 0)))) ? 1'b1 : 1'b0;
 
-    always @(*) begin
-        if (mem_hazard || ((rs1_hazard || rs2_hazard) && (IF_ID_opcode != `B_T)))
-            stall = 1;
-        else
-            stall = 0;
-    end
+    assign rs2_hazard = ((ID_EX_MemRead && (ID_EX_rs2 != 0) && 
+                        ((ID_EX_rs2 == IF_ID_rs1) || (ID_EX_rs2 == IF_ID_rs2))) ||
+                        (IF_ID_MemRead && ((IF_ID_rs1 == IF_ID_rs2) && (IF_ID_rs2 != 0)))) ? 1'b1 : 1'b0;
+
+    assign mem_hazard = (IF_ID_MemRead && ((IF_ID_rs1 == IF_ID_rs2) && (IF_ID_rs1 != 0))) ? 1'b1 : 1'b0;
+
+    assign stall = (mem_hazard || ((rs1_hazard || rs2_hazard) && (IF_ID_opcode != `B_T))) ? 1'b1 : 1'b0;
 
 endmodule
