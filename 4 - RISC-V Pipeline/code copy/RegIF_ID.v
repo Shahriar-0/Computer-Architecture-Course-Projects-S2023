262c11696cf1d8bf52f2b4f1c27b676f2d79c929
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2023-06-07 22:43:45 +0330

    RegIF_ID

--- 4 - RISC-V Pipeline/code copy/RegIF_ID.v
+++ 4 - RISC-V Pipeline/code copy/RegIF_ID.v
@@ -1,40 +1,28 @@
-module RegIF_ID(clk, rst, instruction, OPCode, 
-                func3, func7, RS1, RS2, RD, RDOut,
-                PCOut, func7Out, func3Out, PC,
-                instructionOut, RS1Out, RS2Out);
+module RegIF_ID(clk, rst, en, clr instrF, PCF, 
+                PCPlus4F, PCPlus4D,
+                instrD, PCD);
     
-    input clk, rst;
-    input [31:0] instruction, PC;
-    input [2:0] OPCode, func3;
-    input [6:0] func7;
-    input [4:0] RS1, RS2, RD;
+    input clk, rst, clr, en;
+    input [31:0] instrF, PCF, PCPlus4F;
+    output reg [31:0] instrD, PCD, PCPlus4D;
 
-
-    output reg [31:0] instructionOut, PCOut;
-    output reg [2:0] OPCodeOut, func3Out;
-    output reg [6:0] func7Out;
-    output reg [4:0] RS1Out, RS2Out, RDOut;
-
+    
+    
     always @(posedge clk or posedge rst) begin
         if (rst) begin
-            instructionOut <= 32'b0;
-            PCOut <= 32'b0;
-            OPCodeOut <= 3'b0;
-            func7Out <= 7'b0;
-            func3Out <= 3'b0;
-            RS1Out <= 5'b0;
-            RS2Out <= 5'b0;
-            RDOut <= 5'b0;
+            instrD <= 32'b0;
+            PCD <= 32'b0;
+            PCPlus4D <= 3'b0;
         end 
-        else begin
-            instructionOut <= instruction;
-            PCOut <= PC;
-            OPCodeOut <= OPCode;
-            func7Out <= func7;
-            func3Out <= func3;
-            RS1Out <= RS1;
-            RS2Out <= RS2;
-            RD1Out <= RD;
+        else if (clr) begin
+            instrD <= 32'b0;
+            PCD <= 32'b0;
+            PCPlus4D <= 3'b0;
+        end 
+        else if(~en) begin
+            instrD <= instrF;
+            PCD <= PCF;
+            PCPlus4D <= PCPlus4D;
         end
     end
 
