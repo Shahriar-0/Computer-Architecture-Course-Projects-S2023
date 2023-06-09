4e7223625ebb7f14f40e8500c5333a89e578a5cf
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2023-06-09 16:07:24 +0330

    .

--- 4 - RISC-V Pipeline/code/RISC_V_Controller.v
+++ 4 - RISC-V Pipeline/code/RISC_V_Controller.v
@@ -1,12 +1,12 @@
 module RISC_V_Controller(clk, rst, op, func3, func7,
                          regWriteD, resultSrcD, memWriteD,
                          jumpD, branchD, ALUControlD,
-                         ALUSrcD, immSrcD);
+                         ALUSrcD, immSrcD, luiD);
     input [6:0] op;
     input [2:0] func3;
     input clk, rst, func7;
     
-    output ALUSrcD, memWriteD, regWriteD;
+    output ALUSrcD, memWriteD, regWriteD, luiD;
     output [1:0] resultSrcD, jumpD;
     output [2:0] ALUControlD, immSrcD,branchD;
 
@@ -16,7 +16,7 @@ module RISC_V_Controller(clk, rst, op, func3, func7,
         .op(op), .func3(func3), .regWriteD(regWriteD), 
         .resultSrcD(resultSrcD), .memWriteD(memWriteD),
         .jumpD(jumpD), .branchD(branchD), .ALUOp(ALUOp), 
-        .ALUSrcD(ALUSrcD), .immSrcD(immSrcD) 
+        .ALUSrcD(ALUSrcD), .immSrcD(immSrcD, .luiD(luiD)
     );
     
     ALU_Controller ALUdecoder(
