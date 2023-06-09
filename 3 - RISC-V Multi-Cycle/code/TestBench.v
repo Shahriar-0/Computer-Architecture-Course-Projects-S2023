38ffca93fcc7d5e7e6ca8999fd8def8897e3da44
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2023-06-09 13:01:12 +0330

    add testbench for multicycle

---
+++ 3 - RISC-V Multi-Cycle/code/TestBench.v
@@ -0,0 +1,13 @@
+module TB();
+    reg clk, rst;
+    RISC_V risc_v(.clk(clk), .rst(rst));
+    always #5 clk = ~clk;
+
+    initial begin
+        clk = 1'b0;
+        #2 rst = 1'b1;
+        #6 rst = 1'b0;
+        #3500 $stop;
+    end
+    
+endmodule
