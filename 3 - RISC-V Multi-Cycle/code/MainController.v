c3c00911a370e452d27232de22a3fde3f2bae5f3
Shahriar-0 <attarshahriar@gmail.com>
2023-06-22 15:15:40 +0330

    new controller

--- 3 - RISC-V Multi-Cycle/code/MainController.v
+++ 3 - RISC-V Multi-Cycle/code/MainController.v
@@ -7,25 +7,24 @@
 `define LW_T    7'b0000011
 `define JALR_T  7'b1100111
 
-
-`define IF     5'b00000
-`define ID     5'b00001
-`define EX1    5'b00010
-`define EX2    5'b00011
-`define EX3    5'b00100
-`define EX4    5'b00101
-`define EX5    5'b00110
-`define EX6    5'b00111
-`define EX7    5'b01000
-`define EX8    5'b01001
-`define EX9    5'b01010
-`define MEM1   5'b01011
-`define MEM2   5'b01100
-`define MEM3   5'b01101
-`define MEM4   5'b01110
-`define MEM5   5'b01111
-`define MEM6   5'b10000
-`define WB     5'b10001
+`define IF       5'b00000
+`define ID       5'b00001
+`define EX_I     5'b00010
+`define MEM_I    5'b01100
+`define EX_R     5'b00011
+`define MEM_R    5'b01110
+`define EX_B     5'b00100
+`define EX_J     5'b00101
+`define MEM_J    5'b01000
+`define WB_J     5'b10000
+`define EX_JALR  5'b01001
+`define MEM_JALR 5'b00110
+`define EX_S     5'b00111
+`define MEM_S    5'b01101
+`define EX_LW    5'b01010
+`define MEM_LW   5'b01011
+`define WB_LW    5'b10001
+`define EX_U     5'b01111
 
 module MainController(clk, rst, op, zero, 
                       PCUpdate, adrSrc, memWrite, branch,
@@ -44,34 +43,40 @@ module MainController(clk, rst, op, zero,
 
     always @(pstate or op) begin
         case(pstate)
-            `IF : nstate <= `ID;
+            `IF  : nstate <= `ID;
 
-            `ID: nstate <= (op == `R_T) ? `EX2 :
-                           (op == `I_T) ? `EX1 :
-                           (op == `S_T) ? `EX6 :
-                           (op == `J_T) ? `EX4 :
-                           (op == `B_T) ? `EX3 :
-                           (op == `U_T) ? `MEM5 :
-                           (op == `LW_T) ? `EX9 :
-                           (op == `JALR_T) ? `EX8: `IF;
+            `ID  : nstate <= (op == `I_T)    ? `EX_I     :
+                             (op == `R_T)    ? `EX_R     :
+                             (op == `B_T)    ? `EX_B     :
+                             (op == `J_T)    ? `EX_J     :
+                             (op == `U_T)    ? `EX_U     :   
+                             (op == `S_T)    ? `EX_S     :
+                             (op == `JALR_T) ? `EX_JALR  :
+                             (op == `LW_T)   ? `EX_LW    : `IF; // undefined instruction
 
-            `EX1 : nstate <= `MEM2;
-            `EX2 : nstate <= `MEM4;
-            `EX3 : nstate <= `IF;
-            `EX4 : nstate <= `EX7;
-            `EX5 : nstate <= `MEM2;
-            `EX6 : nstate <= `MEM3;
-            `EX7 : nstate <= `MEM6;
-            `EX8 : nstate <= `EX5;
-            `EX9 : nstate <= `MEM1;
-            `MEM1: nstate <= `WB;
-            `MEM2: nstate <= `IF;
-            `MEM3: nstate <= `IF;
-            `MEM4: nstate <= `IF;
-            `MEM5: nstate <= `IF;
-            `MEM6: nstate <= `IF;
+            `EX_I : nstate <= `MEM_I;
+            `MEM_I: nstate <= `IF;
 
-            `WB  : nstate <= `IF;
+            `EX_R : nstate <= `MEM_R;
+            `MEM_R: nstate <= `IF;
+
+            `EX_B : nstate <= `IF;
+
+            `EX_J : nstate <= `MEM_J;
+            `MEM_J : nstate <= `WB_J;
+            `WB_J: nstate <= `IF;
+
+            `EX_S : nstate <= `MEM_S;
+            `MEM_S: nstate <= `IF;
+            
+            `EX_JALR : nstate <= `MEM_JALR;
+            `MEM_JALR : nstate <= `MEM_I;
+
+            `EX_LW : nstate <= `MEM_LW;
+            `MEM_LW: nstate <= `WB_LW;
+            `WB_LW  : nstate <= `IF;
+
+            `EX_U: nstate <= `IF;
         endcase
     end
 
@@ -82,67 +87,94 @@ module MainController(clk, rst, op, zero,
                 regWrite, PCUpdate, branch, IRWrite} <= 14'b0;
 
         case(pstate)
-
+            // instruction fetch
             `IF : begin
                 IRWrite   <= 1'b1;
+                adrSrc    <= 1'b0;
                 ALUSrcA   <= 2'b00;
                 ALUSrcB   <= 2'b10;
                 ALUOp     <= 2'b00;
                 resultSrc <= 2'b10;
                 PCUpdate  <= 1'b1;
-                adrSrc = 1'b0;
             end
-        
+            // instruction decode
             `ID: begin
                 ALUSrcA   <= 2'b01;
                 ALUSrcB   <= 2'b01;
                 ALUOp     <= 2'b00;
                 immSrc    <= 3'b010;
             end
-        
-            `EX1: begin 
+            // I-type
+            `EX_I: begin 
                 ALUSrcA   <= 2'b10;
                 ALUSrcB   <= 2'b01;
                 immSrc    <= 3'b000;
                 ALUOp     <= 2'b11;
             end
 
-            `EX2: begin
-                ALUSrcA   <= 2'b10;
-                ALUSrcB   <= 2'b00;
-                ALUOp     <= 2'b10;
+            `MEM_I: begin
+                resultSrc <= 2'b00;
+                regWrite  <= 1'b1;
             end
-        
-            `EX3: begin
+            // JALR-type (it's different from I-type so in another cycle it's handled)
+            `EX_JALR: begin 
                 ALUSrcA   <= 2'b10;
-                ALUSrcB   <= 2'b00;
-                ALUOp     <= 2'b01;
-                resultSrc <= 2'b0;
-                branch    <= 1'b1;
-            end
-        
-            `EX4: begin
-                ALUSrcA   <= 2'b01;
-                ALUSrcB   <= 2'b10;
+                ALUSrcB   <= 2'b01;
+                immSrc    <= 3'b000;
                 ALUOp     <= 2'b00;
             end
-        
-            `EX5: begin
+
+            `MEM_JALR: begin
                 ALUSrcA   <= 2'b01;
                 ALUSrcB   <= 2'b10;
                 ALUOp     <= 2'b00;
                 resultSrc <= 2'b00;
                 PCUpdate  <= 1'b1;
             end
-        
-            `EX6: begin
+            // LW (like JALR)
+            `EX_LW: begin 
                 ALUSrcA   <= 2'b10;
                 ALUSrcB   <= 2'b01;
+                immSrc    <= 3'b000;
                 ALUOp     <= 2'b00;
-                immSrc    <= 3'b001;
+            end
+
+            `MEM_LW: begin
+                resultSrc <= 2'b00;
+                adrSrc    <= 1'b1;
+            end
+
+            `WB_LW: begin
+                resultSrc <= 2'b01;
+                regWrite  <= 1'b1;
+            end
+            // T-type
+            `EX_R: begin
+                ALUSrcA   <= 2'b10;
+                ALUSrcB   <= 2'b00;
+                ALUOp     <= 2'b10;
+            end
+
+            `MEM_R: begin
+                resultSrc <= 2'b00;
+                regWrite  <= 1'b1;
+            end
+            // B-type
+            `EX_B: begin
+                ALUSrcA   <= 2'b10;
+                ALUSrcB   <= 2'b00;
+                ALUOp     <= 2'b01;
+                resultSrc <= 2'b00;
+                branch    <= 1'b1;
+            end
+            // J-type
+            `EX_J: begin
+                ALUSrcA   <= 2'b01;
+                ALUSrcB   <= 2'b10;
+                ALUOp     <= 2'b00;
             end
         
-            `EX7: begin
+            `MEM_J: begin
                 regWrite  <= 1'b1;
                 ALUSrcA   <= 2'b01;
                 ALUSrcB   <= 2'b01;
@@ -150,57 +182,29 @@ module MainController(clk, rst, op, zero,
                 ALUOp     <= 2'b00;
             end
 
-            `EX8: begin 
+            `WB_J: begin
+                resultSrc <= 2'b00;
+                PCUpdate  <= 1'b1;
+            end
+            // S-type
+            `EX_S: begin
                 ALUSrcA   <= 2'b10;
                 ALUSrcB   <= 2'b01;
-                immSrc    <= 3'b000;
                 ALUOp     <= 2'b00;
-            end
-
-            `EX9: begin 
-                ALUSrcA   <= 2'b10;
-                ALUSrcB   <= 2'b01;
-                immSrc    <= 3'b000;
-                ALUOp     <= 2'b00;
-            end
-
-            `MEM1: begin
-                resultSrc <= 2'b00;
-                adrSrc    <= 1'b1;
+                immSrc    <= 3'b001;
             end
         
-            `MEM2: begin
-                resultSrc <= 2'b00;
-                regWrite  <= 1'b1;
-            end
-        
-            `MEM3: begin
+            `MEM_S: begin
                 resultSrc <= 2'b00;
                 adrSrc    <= 1'b1;
                 memWrite  <= 1'b1;
             end
-        
-            `MEM4: begin
-                resultSrc <= 2'b00;
-                regWrite  <= 1'b1;
-            end
-        
-            `MEM5: begin
+            // U-type
+            `EX_U: begin
                 resultSrc <= 2'b11;
                 immSrc    <= 3'b100;
                 regWrite  <= 1'b1;
             end
-        
-            `MEM6: begin
-                resultSrc <= 2'b00;
-                PCUpdate  <= 1'b1;
-            end
-        
-            `WB: begin
-                resultSrc <= 2'b01;
-                regWrite  <= 1'b1;
-                end
-        
         endcase
     end
 
